4b52c2796f3e53ea530d5423511c70d91d979df4
cgtechie <clargohtech@gmail.com>
2025-02-20 20:26:49 +0800

    removed connection parameters

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-conn = st.connection("connections.snowflake", type="snowflake")
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
